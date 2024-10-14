7e53d33231859795358f547c1783e7c37d5aa68b
nivasKodavaty <85055037+nivasKodavaty@users.noreply.github.com>
2024-10-14 10:11:34 +0530

    Improvised the code

--- src/main/java/org/example/IPLAnalysis.java
+++ src/main/java/org/example/IPLAnalysis.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
@@ -12,35 +13,42 @@ public class IPLAnalysis {
     public static void main(String[] args) {
         String matchesFile = "/Users/nivaskodavaty/Desktop/IPL-2/src/data/IPL Matches.csv";
         String deliveriesFile = "/Users/nivaskodavaty/Desktop/IPL-2/src/data/deliveries.csv";
+        CSVReader matchesReader;
+        CSVReader deliveriesReader ;
+        CSVReader matchesReader1;
+        CSVReader deliveriesReader2 ;
 
         try {
 
-            System.out.println(getMatchesPlayedPerYear(matchesFile));
-            System.out.println(getMatchesWonByTeam(matchesFile));
-            System.out.println(runsConceded(deliveriesFile));
-            System.out.println(economicalBowlers(deliveriesFile));
+            matchesReader = new CSVReader(new FileReader(matchesFile));
+            deliveriesReader = new CSVReader(new FileReader(deliveriesFile));
+            matchesReader1 = new CSVReader(new FileReader(matchesFile));
+            deliveriesReader2 = new CSVReader(new FileReader(deliveriesFile));
+            System.out.println(matchesPlayed(matchesReader));
+            System.out.println(MatchesWonByTeam(matchesReader1));
+            System.out.println(runsConceded(deliveriesReader));
+            System.out.println(economicalBowlers(deliveriesReader2));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static Map<Integer, Integer> getMatchesPlayedPerYear(String matchesFile) throws IOException, CsvValidationException {
+    public static Map<Integer, Integer> matchesPlayed(CSVReader reader) throws CsvValidationException, IOException {
         Map<Integer, Integer> matchesPerYear = new HashMap<>();
-        try (CSVReader reader = new CSVReader(new FileReader(matchesFile))) {
-            String[] nextLine;
-            reader.readNext();
-            while ((nextLine = reader.readNext()) != null) {
-                int season = Integer.parseInt(nextLine[1]);
-                matchesPerYear.put(season, matchesPerYear.getOrDefault(season, 0) + 1);
-            }
+        String[] nextLine;
+        reader.readNext();
+        while ((nextLine = reader.readNext()) != null) {
+            int season = Integer.parseInt(nextLine[1]);
+            matchesPerYear.put(season, matchesPerYear.getOrDefault(season, 0) + 1);
         }
+
         return matchesPerYear;
+
     }
 
-    public static Map<String, Integer> getMatchesWonByTeam(String matchesFile) throws IOException, CsvValidationException {
+    public static Map<String, Integer> MatchesWonByTeam(CSVReader reader) throws IOException, CsvValidationException {
         Map<String, Integer> matchesWonByTeam = new HashMap<>();
-        try (CSVReader reader = new CSVReader(new FileReader(matchesFile))) {
             String[] nextLine;
             reader.readNext();
             while ((nextLine = reader.readNext()) != null) {
@@ -49,49 +57,57 @@ public class IPLAnalysis {
                     matchesWonByTeam.put(winner, matchesWonByTeam.getOrDefault(winner, 0) + 1);
                 }
             }
-        }
+
 
         return matchesWonByTeam;
     }
 
-    public static Map<String, Integer> runsConceded(String delieversFile) {
+    public static Map<String, Integer> runsConceded(CSVReader reader) throws IOException, CsvException {
         Map<String, Integer> map = new HashMap<>();
-        try (CSVReader reader = new CSVReader(new FileReader(delieversFile))) {
-            List<String[]> list = reader.readAll();
-            list.remove(0);
-            for (String[] strings : list) {
-                if (Integer.parseInt(strings[0]) > 577 && Integer.parseInt(strings[16]) > 0) {
-                    String team = strings[3];
-                    int extraruns = Integer.parseInt(strings[16]);
-                    if (map.containsKey(team)) {
-                        map.put(team, map.get(team) + extraruns);
-                    } else {
-                        map.put(team, extraruns);
-                    }
+        String[] nextLine;
+        reader.readNext();
+        while ((nextLine= reader.readNext())!=null){
+            if (Integer.parseInt(nextLine[0]) > 577 && Integer.parseInt(nextLine[16]) > 0) {
+                String team = nextLine[3];
+                int extraruns = Integer.parseInt(nextLine[16]);
+                if (map.containsKey(team)) {
+                    map.put(team, map.get(team) + extraruns);
+                } else {
+                    map.put(team, extraruns);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+//            List<String[]> list = reader.readAll();
+//            list.remove(0);
+//            for (String[] nextLine : list) {
+//                if (Integer.parseInt(nextLine[0]) > 577 && Integer.parseInt(nextLine[16]) > 0) {
+//                    String team = nextLine[3];
+//                    int extraruns = Integer.parseInt(nextLine[16]);
+//                    if (map.containsKey(team)) {
+//                        map.put(team, map.get(team) + extraruns);
+//                    } else {
+//                        map.put(team, extraruns);
+//                    }
+//                }
+//            }
+
         return map;
     }
 
-    public static Map<String, Double> economicalBowlers(String delieversFile) {
+    public static Map<String, Double> economicalBowlers(CSVReader reader) throws IOException, CsvException {
         Map<String, Integer> runs_Conceded = new HashMap<>();
-        Map<String, Integer> ball_bowled = new HashMap<>();
-        try (CSVReader reader = new CSVReader(new FileReader(delieversFile))) {
+        Map<String, Integer> balls_bowled = new HashMap<>();
             List<String[]> list = reader.readAll();
             list.remove(0);
-
             for (String[] strings : list) {
                 int match_id = Integer.parseInt(strings[0]);
                 int total_runs = Integer.parseInt(strings[17]);
                 String bowler = strings[8];
                 if ((match_id >= 518 && match_id <= 576)) {
-                    if (ball_bowled.containsKey(bowler)) {
-                        ball_bowled.put(bowler, ball_bowled.get(bowler) + 1);
+                    if (balls_bowled.containsKey(bowler)) {
+                        balls_bowled.put(bowler, balls_bowled.get(bowler) + 1);
                     } else {
-                        ball_bowled.put(bowler, 1);
+                        balls_bowled.put(bowler, 1);
                     }
                 }
 
@@ -103,22 +119,23 @@ public class IPLAnalysis {
                     }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        HashMap<String, Double> economy = new HashMap<>();
-        for (String s : ball_bowled.keySet()) {
-            double overs = (double) ball_bowled.get(s) / 6;
+        TreeMap<String, Double> economy = new TreeMap<>();
+        for (String s : balls_bowled.keySet()) {
+            double overs = (double) balls_bowled.get(s) / 6;
             economy.put(s, (double) runs_Conceded.get(s) / overs);
 
         }
-        HashMap<String,Double> sortedAndtrimmed = new HashMap<>();
-        ArrayList<Map.Entry<String, Double>> list = new ArrayList<>(economy.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-        for (int i = 0; i < Math.min(10, list.size()); i++) {
-            Map.Entry<String, Double> entry = list.get(i);
+        HashMap<String, Double> sortedAndtrimmed = new HashMap<>();
+        int count=0;
+        for(Map.Entry<String, Double> entry : economy.entrySet()){
+            if (count>10){
+                break;
+            }
             sortedAndtrimmed.put(entry.getKey(), entry.getValue());
+            count++;
+
         }
+
         return sortedAndtrimmed;
     }
 
