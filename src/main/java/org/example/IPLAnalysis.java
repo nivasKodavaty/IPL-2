9de5a01a402208e537a928f6bfe9cdc3b12a2222
nivasKodavaty <85055037+nivasKodavaty@users.noreply.github.com>
2024-10-16 18:08:30 +0530

    added matches class

--- src/main/java/org/example/IPLAnalysis.java
+++ src/main/java/org/example/IPLAnalysis.java
@@ -9,56 +9,70 @@ import java.io.IOException;
 import java.util.*;
 
 public class IPLAnalysis {
+    static String matchesFile = "/Users/nivaskodavaty/Desktop/IPL-2/src/data/IPL Matches.csv";
+    static String deliveriesFile = "/Users/nivaskodavaty/Desktop/IPL-2/src/data/deliveries.csv";
 
     public static void main(String[] args) {
-        String matchesFile = "/Users/nivaskodavaty/Desktop/IPL-2/src/data/IPL Matches.csv";
-        String deliveriesFile = "/Users/nivaskodavaty/Desktop/IPL-2/src/data/deliveries.csv";
+        ArrayList<Matches> matchesData = getMatchesData();
+        System.out.println(getMatchesPlayed(matchesData));
+        System.out.println(getMatchesWonByTeam(matchesData));
+
+
+    }
+
+    public static ArrayList<Matches> getMatchesData() {
+        ArrayList<Matches> matchesArrayList = new ArrayList<>();
         CSVReader matchesReader;
-        CSVReader deliveriesReader ;
-        CSVReader matchesReader1;
-        CSVReader deliveriesReader2 ;
-
         try {
-
             matchesReader = new CSVReader(new FileReader(matchesFile));
-            deliveriesReader = new CSVReader(new FileReader(deliveriesFile));
-            matchesReader1 = new CSVReader(new FileReader(matchesFile));
-            deliveriesReader2 = new CSVReader(new FileReader(deliveriesFile));
-            System.out.println(matchesPlayed(matchesReader));
-            System.out.println(MatchesWonByTeam(matchesReader1));
-            System.out.println(runsConceded(deliveriesReader));
-            System.out.println(economicalBowlers(deliveriesReader2));
+            matchesReader.readNext();
+            String[] nextLine;
+            while ((nextLine = matchesReader.readNext()) != null) {
+                Matches matchData = new Matches();
+                matchData.setId(Integer.parseInt(nextLine[0]));
+                matchData.setSeason(Integer.parseInt(nextLine[1]));
+                matchData.setCity(nextLine[2]);
+                matchData.setDate(nextLine[3]);
+                matchData.setTeam_1(nextLine[4]);
+                matchData.setTeam_2(nextLine[5]);
+                matchData.setToss_winner(nextLine[6]);
+                matchData.setToss_decision(nextLine[7]);
+                matchData.setResult(nextLine[8]);
+                matchData.setDl_applied(Integer.parseInt(nextLine[9]));
+                matchData.setWinner(nextLine[10]);
+                matchData.setWin_by_runs(Integer.parseInt(nextLine[11]));
+                matchData.setWin_by_runs(Integer.parseInt(nextLine[12]));
+                matchData.setPlayer_of_match(nextLine[13]);
+                matchData.setVenue(nextLine[14]);
+                matchData.setUmpire1(nextLine[15]);
+                matchData.setUmpire2(nextLine[16]);
+                matchesArrayList.add(matchData);
 
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return matchesArrayList;
     }
 
-    public static Map<Integer, Integer> matchesPlayed(CSVReader reader) throws CsvValidationException, IOException {
-        Map<Integer, Integer> matchesPerYear = new HashMap<>();
-        String[] nextLine;
-        reader.readNext();
-        while ((nextLine = reader.readNext()) != null) {
-            int season = Integer.parseInt(nextLine[1]);
-            matchesPerYear.put(season, matchesPerYear.getOrDefault(season, 0) + 1);
+    public static HashMap<Integer, Integer> getMatchesPlayed(ArrayList<Matches> matchesData) {
+        HashMap<Integer, Integer> matchesPlayedPerYear = new HashMap<>();
+        for (Matches matches : matchesData) {
+            int season = matches.getSeason();
+            matchesPlayedPerYear.put(season, matchesPlayedPerYear.getOrDefault(season, 0) + 1);
         }
 
-        return matchesPerYear;
+        return matchesPlayedPerYear;
 
     }
 
-    public static Map<String, Integer> MatchesWonByTeam(CSVReader reader) throws IOException, CsvValidationException {
-        Map<String, Integer> matchesWonByTeam = new HashMap<>();
-            String[] nextLine;
-            reader.readNext();
-            while ((nextLine = reader.readNext()) != null) {
-                String winner = nextLine[10];
-                if (!winner.isEmpty()) {
-                    matchesWonByTeam.put(winner, matchesWonByTeam.getOrDefault(winner, 0) + 1);
-                }
-            }
-
-
+    public static HashMap<String, Integer> getMatchesWonByTeam(ArrayList<Matches> matchesData) {
+        HashMap<String, Integer> matchesWonByTeam = new HashMap<>();
+        for (Matches matches : matchesData) {
+            String winner = matches.getWinner();
+            matchesWonByTeam.put(winner, matchesWonByTeam.getOrDefault(winner, 0) + 1);
+        }
         return matchesWonByTeam;
     }
 
@@ -66,7 +80,7 @@ public class IPLAnalysis {
         Map<String, Integer> map = new HashMap<>();
         String[] nextLine;
         reader.readNext();
-        while ((nextLine= reader.readNext())!=null){
+        while ((nextLine = reader.readNext()) != null) {
             if (Integer.parseInt(nextLine[0]) > 577 && Integer.parseInt(nextLine[16]) > 0) {
                 String team = nextLine[3];
                 int extraruns = Integer.parseInt(nextLine[16]);
@@ -77,36 +91,34 @@ public class IPLAnalysis {
                 }
             }
         }
-
-
         return map;
     }
 
     public static Map<String, Double> economicalBowlers(CSVReader reader) throws IOException, CsvException {
         Map<String, Integer> runs_Conceded = new HashMap<>();
         Map<String, Integer> balls_bowled = new HashMap<>();
-            List<String[]> list = reader.readAll();
-            list.removeFirst();
-            for (String[] strings : list) {
-                int match_id = Integer.parseInt(strings[0]);
-                int total_runs = Integer.parseInt(strings[17]);
-                String bowler = strings[8];
-                if ((match_id >= 518 && match_id <= 576)) {
-                    if (balls_bowled.containsKey(bowler)) {
-                        balls_bowled.put(bowler, balls_bowled.get(bowler) + 1);
-                    } else {
-                        balls_bowled.put(bowler, 1);
-                    }
-                }
-
-                if ((match_id >= 518 && match_id <= 576) && total_runs > 0) {
-                    if (runs_Conceded.containsKey(bowler)) {
-                        runs_Conceded.put(bowler, runs_Conceded.get(bowler) + total_runs);
-                    } else {
-                        runs_Conceded.put(bowler, total_runs);
-                    }
+        List<String[]> list = reader.readAll();
+        list.removeFirst();
+        for (String[] strings : list) {
+            int match_id = Integer.parseInt(strings[0]);
+            int total_runs = Integer.parseInt(strings[17]);
+            String bowler = strings[8];
+            if ((match_id >= 518 && match_id <= 576)) {
+                if (balls_bowled.containsKey(bowler)) {
+                    balls_bowled.put(bowler, balls_bowled.get(bowler) + 1);
+                } else {
+                    balls_bowled.put(bowler, 1);
                 }
             }
+
+            if ((match_id >= 518 && match_id <= 576) && total_runs > 0) {
+                if (runs_Conceded.containsKey(bowler)) {
+                    runs_Conceded.put(bowler, runs_Conceded.get(bowler) + total_runs);
+                } else {
+                    runs_Conceded.put(bowler, total_runs);
+                }
+            }
+        }
         TreeMap<String, Double> economy = new TreeMap<>();
         for (String s : balls_bowled.keySet()) {
             double overs = (double) balls_bowled.get(s) / 6;
@@ -114,9 +126,9 @@ public class IPLAnalysis {
 
         }
         HashMap<String, Double> sortedAndtrimmed = new HashMap<>();
-        int count=0;
-        for(Map.Entry<String, Double> entry : economy.entrySet()){
-            if (count>10){
+        int count = 0;
+        for (Map.Entry<String, Double> entry : economy.entrySet()) {
+            if (count > 10) {
                 break;
             }
             sortedAndtrimmed.put(entry.getKey(), entry.getValue());
---
+++ src/main/java/org/example/Matches.java
@@ -0,0 +1,161 @@
+package org.example;
+
+
+
+public class Matches {
+    private int id;
+    private int season;
+    private String city;
+    private String date;
+    private String team_1;
+    private String team_2;
+    private String toss_winner;
+    private String toss_decision;
+    private String result;
+    private int dl_applied;
+    private String winner;
+    private int win_by_runs;
+    private int win_by_wickets;
+    private String player_of_match;
+    private String venue;
+    private String umpire1;
+    private String umpire2;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String data) {
+        this.date = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSeason() {
+        return season;
+    }
+
+    public void setSeason(int season) {
+        this.season = season;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTeam_1() {
+        return team_1;
+    }
+
+    public void setTeam_1(String team_1) {
+        this.team_1 = team_1;
+    }
+
+    public String getTeam_2() {
+        return team_2;
+    }
+
+    public void setTeam_2(String team_2) {
+        this.team_2 = team_2;
+    }
+
+    public String getToss_winner() {
+        return toss_winner;
+    }
+
+    public void setToss_winner(String toss_winner) {
+        this.toss_winner = toss_winner;
+    }
+
+    public String getToss_decision() {
+        return toss_decision;
+    }
+
+    public void setToss_decision(String toss_decision) {
+        this.toss_decision = toss_decision;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getDl_applied() {
+        return dl_applied;
+    }
+
+    public void setDl_applied(int dl_applied) {
+        this.dl_applied = dl_applied;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public int getWin_by_runs() {
+        return win_by_runs;
+    }
+
+    public void setWin_by_runs(int win_by_runs) {
+        this.win_by_runs = win_by_runs;
+    }
+
+    public int getWin_by_wickets() {
+        return win_by_wickets;
+    }
+
+    public void setWin_by_wickets(int win_by_wickets) {
+        this.win_by_wickets = win_by_wickets;
+    }
+
+    public String getPlayer_of_match() {
+        return player_of_match;
+    }
+
+    public void setPlayer_of_match(String player_of_match) {
+        this.player_of_match = player_of_match;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getUmpire1() {
+        return umpire1;
+    }
+
+    public void setUmpire1(String umpire1) {
+        this.umpire1 = umpire1;
+    }
+
+    public String getUmpire2() {
+        return umpire2;
+    }
+
+    public void setUmpire2(String umpire2) {
+        this.umpire2 = umpire2;
+    }
+
+
+}
